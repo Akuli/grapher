5a170f90de149b7437bf107edc52000caf4bbd2a
Akuli <akuviljanen17@gmail.com>
2018-11-03 16:26:37 +0200

    add some code for parsing math strings like "2x^2-x+8"

---
+++ js/math-parser.js
@@ -0,0 +1,262 @@
+define([], function() {
+  "use strict";
+
+  const FUNCTIONS = {
+    sqrt: Math.sqrt,
+    abs: Math.abs,
+    cbrt: Math.cbrt
+  };
+
+  const CONSTANTS = {
+    e: Math.E,
+    pi: Math.PI
+  };
+
+  const createKeyRegex = obj => new RegExp('^(' + Object.getOwnPropertyNames(obj).join('|') + ')');
+  const TOKEN_SPEC = [
+    { name: 'number', regex: /^[0-9]+\.[0-9]+/ },
+    { name: 'number', regex: /^[0-9]+\./ },
+    { name: 'number', regex: /^\.[0-9]+/ },
+    { name: 'number', regex: /^[0-9]+/ },
+    { name: 'function', regex: createKeyRegex(FUNCTIONS) },
+    { name: 'constant', regex: createKeyRegex(CONSTANTS) },
+    { name: 'var', regex: /^\w/ },
+    { name: 'operator', regex: /^[+\-*/^|()]/ },
+    { name: 'space', regex: /^\s+/ }
+  ];
+
+  function* tokenizeExpression(mathString) {
+    while (mathString !== "") {
+      let matched = false;
+      for (let spec of TOKEN_SPEC) {
+        if (spec.regex.test(mathString)) {
+          const string = spec.regex.exec(mathString)[0];
+          mathString = mathString.slice(string.length);
+          if (spec.name !== 'space') {
+            yield { type: spec.name, value: string };
+          }
+          matched = true;
+        }
+      }
+
+      if (!matched) {
+        throw new Error("I don't know what " + mathString + " means");
+      }
+    }
+  }
+
+  function tokenMatches(token, props) {
+    for (let key of Object.getOwnPropertyNames(props)) {
+      if (token[key] !== props[key]) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  class TokenIterator {
+    constructor(generator) {
+      this._generator = generator;
+      this._comingSoon = null;
+    }
+
+    _callNext(errorOnEOF) {
+      const next = this._generator.next();
+      if (next.done) {
+        if (errorOnEOF) {
+          throw new Error("unexpected end of math");
+        }
+        return null;
+      }
+      if (next.value === null) {
+        throw new Error("unexpected null");
+      }
+      return next.value;
+    }
+
+    eof() {
+      if (this._comingSoon !== null) {
+        return false;
+      }
+      this._comingSoon = this._callNext(false);
+      return (this._comingSoon === null);
+    }
+
+    comingUp(props) {
+      if (this._comingSoon === null) {
+        this._comingSoon = this._callNext(true);
+      }
+      return tokenMatches(this._comingSoon, props);
+    }
+
+    nextToken(props) {
+      let result;
+      if (this._comingSoon === null) {
+        result = this._callNext(true);
+      } else {
+        result = this._comingSoon;
+        this._comingSoon = null;
+      }
+
+      if (!tokenMatches(result, props)) {
+        throw new Error("unexpected token: " + result.value);
+      }
+      return result;
+    }
+  }
+
+  class Parser {
+    constructor(tokenGenerator, allowedVarNames) {
+      this.iter = new TokenIterator(tokenGenerator);
+      this.varNames = allowedVarNames;
+    }
+
+    expressionComingUp() {
+      return (
+        this.iter.comingUp({ type: 'operator', value: '+' }) ||
+        this.iter.comingUp({ type: 'operator', value: '-' }) ||
+        this.iter.comingUp({ type: 'var' }) ||
+        this.iter.comingUp({ type: 'function' }) ||
+        this.iter.comingUp({ type: 'operator', value: '(' }) ||
+        this.iter.comingUp({ type: 'number' }));
+    }
+
+    parseExpressionWithoutBinaryOperators() {
+      if (this.iter.comingUp({ type: 'var' })) {
+        const varName = this.iter.nextToken({ type: 'var' }).value;
+        if (!this.varNames.includes(varName)) {
+          throw new Error("unknown variable: " + varName);
+        }
+        return { type: 'var', name: varName };
+      }
+      if (this.iter.comingUp({ type: 'constant' })) {
+        const token = this.iter.nextToken({ type: 'constant' });
+        return { type: 'number', value: CONSTANTS[token.value] };
+      }
+      if (this.iter.comingUp({ type: 'function' })) {
+        // this assumes that all functions take exactly 1 argument
+        const functionToken = this.iter.nextToken({ type: 'function' });
+        const arg = this.parseExpression();
+        return { type: 'functionCall', functionName: functionToken.value, arg: arg };
+      }
+      if (this.iter.comingUp({ type: 'operator', value: '(' })) {
+        this.iter.nextToken({ type: 'operator', value: '(' });
+        const result = this.parseExpression();
+        this.iter.nextToken({ type: 'operator', value: ')' });
+        return result;
+      }
+      if (this.iter.comingUp({ type: 'number' })) {
+        return { type: 'number', value: +this.iter.nextToken({ type: 'number' }).value };
+      }
+      throw new Error("don't know how to parse " + this.iter.nextToken({}).value);
+    }
+
+    binaryOperatorComingUp() {
+      for (let binaryOperator of '+-*/^') {
+        if (this.iter.comingUp({ type: 'operator', value: binaryOperator })) {
+          return true;
+        }
+      }
+      return false;
+    }
+
+    parseExpression() {
+      // this must be here and NOT in parseExpressionWithoutBinaryOperators
+      // because -x^2-y must be always 0-x^2-y, and this is the most robust way
+      // to ensure it i could think of
+      let minusing = false;
+      if (this.iter.comingUp({ type: 'operator', value: '+' })) {
+        this.iter.nextToken({ type: 'operator', value: '+' });
+      } else if (this.iter.comingUp({ type: 'operator', value: '-' })) {
+        this.iter.nextToken({ type: 'operator', value: '-' });
+        minusing = true;
+      }
+
+      // things with even indexes are ast objects
+      // things with odd indexes are operator name strings
+      const funnyStuff = [ this.parseExpressionWithoutBinaryOperators() ];
+
+      if (minusing) {
+        // add "0 -" in front of all the things
+        funnyStuff.splice(0, 0, { type: 'number', value: 0 }, '-');
+      }
+
+      while (!this.iter.eof() && (this.expressionComingUp() || this.binaryOperatorComingUp())) {
+        if (this.binaryOperatorComingUp()) {
+          funnyStuff.push(this.iter.nextToken({ type: 'operator' }).value);
+        } else {
+          funnyStuff.push("*");
+        }
+        funnyStuff.push(this.parseExpressionWithoutBinaryOperators());
+      }
+
+      // replace operators with binaryOp ast nodes
+      for (let operators of ['^', '*/', '+-']) {
+        let indexes = [];
+        for (let op of operators) {
+          funnyStuff.forEach((stringOrAst, index) => {
+            if (stringOrAst === op) {
+              indexes.push(index);
+            }
+          });
+        }
+
+        // the indexes must be sorted if there's more than 1 operator
+        // the indexes must NOT be reversed because then x-y-z would be
+        // interpreted as x-(y-z) instead of (x-y)-z
+        indexes.sort((i, j) => i-j);
+
+        // this is funny and dumb and awesome
+        while (indexes.length !== 0) {
+          const i = indexes.shift();
+          const [ lhs, op, rhs ] = funnyStuff.splice(i-1, 3);
+          const opAst = { type: 'operatorCall', operator: op, lhs: lhs, rhs: rhs };
+          funnyStuff.splice(i-1, 0, opAst);
+
+          // 3 funnyStuff elements were replaced with just 1, so adjust indexes
+          for (let i = 0; i < indexes.length; i++) {
+            indexes[i] -= 2;
+          }
+        }
+      }
+
+      if (funnyStuff.length !== 1) {
+        throw new Error("something weird happened");
+      }
+      return funnyStuff[0];
+    }
+  }
+
+  function parse(mathString, allowedVarNames) {
+    const parser = new Parser(tokenizeExpression(mathString), allowedVarNames);
+    const result = parser.parseExpression();
+    if (!parser.iter.eof()) {
+      throw new Error("the math contains something invalid at the end");
+    }
+    return result;
+  }
+
+  function evaluate(ast, varValues) {
+    switch (ast.type) {
+    case 'operatorCall':
+      switch (ast.operator) {
+      case '+': return evaluate(ast.lhs, varValues) + evaluate(ast.rhs, varValues);
+      case '-': return evaluate(ast.lhs, varValues) - evaluate(ast.rhs, varValues);
+      case '*': return evaluate(ast.lhs, varValues) * evaluate(ast.rhs, varValues);
+      case '/': return evaluate(ast.lhs, varValues) / evaluate(ast.rhs, varValues);
+      case '^': return Math.pow(evaluate(ast.lhs, varValues), evaluate(ast.rhs, varValues));
+      default: throw new Error("unsupported operator: " + ast.operator);
+      }
+    case 'var':
+      return varValues[ast.name];
+    case 'number':
+      return ast.value;
+    case 'functionCall':
+      return FUNCTIONS[ast.functionName](evaluate(ast.arg, varValues));
+    default:
+      throw new Error("unsupported ast expression type " + ast.type);
+    }
+  }
+
+  return { parse: parse, evaluate: evaluate };
+});
